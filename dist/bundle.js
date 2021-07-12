04b4f89da286c3dd707d8365de2dc303544d9e49
386083520 <386083520@qq.com>
2021-07-12 21:55:08 +0800

    使axios具备自己的方法

--- dist/bundle.js
+++ dist/bundle.js
@@ -14,13 +14,33 @@ var axios = (function () {
         console.log('gsd1', config);
     };
 
+    Axios.prototype.get = function request(config) {
+        console.log('gsd1get', config);
+    };
+
+    Axios.prototype.getUri = function getUri(config) {
+        console.log('gsd1getUri', config);
+    };
+
     var Axios_1 = Axios;
+
+    function extend(a, b, thisArg) {
+        Object.keys(b).forEach(item => {
+            a[item] = b[item];
+        });
+    }
+
+    var utils = {
+        extend: extend
+    };
 
     function createInstance(defaultConfig) {
         console.log('gsd2', defaultConfig);
         var context = new Axios_1(defaultConfig);
         // TODO
         var instance = Axios_1.prototype.request.bind(context);
+        utils.extend(instance, Axios_1.prototype);
+        console.dir(instance);
         return instance
     }
     var axios = createInstance(defaults_1);
