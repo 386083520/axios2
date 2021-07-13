1310a66bfa7508a4909dd9fe102eaf11296b9bdd
386083520 <386083520@qq.com>
2021-07-13 22:10:27 +0800

    chain存放拦截器及拦截器实现原理

--- dist/bundle.js
+++ dist/bundle.js
@@ -111,7 +111,20 @@ var axios = (function () {
         console.log('gsd1', config);
         var promise;
         promise = Promise.resolve(config);
+        var requestInterceptorChain = [];
+        this.interceptors.request.handlers.forEach(interceptor => {
+            requestInterceptorChain.unshift(interceptor.fulfilled, interceptor.rejected);
+        });
+        console.log('requestInterceptorChain', requestInterceptorChain);
+        var responseInterceptorChain = [];
+        this.interceptors.response.handlers.forEach(interceptor => {
+            responseInterceptorChain.push(interceptor.fulfilled, interceptor.rejected);
+        });
+        console.log('responseInterceptorChain', responseInterceptorChain);
         var chain = [dispatchRequest, undefined];
+        Array.prototype.unshift.apply(chain, requestInterceptorChain);
+        Array.prototype.push.apply(chain, responseInterceptorChain);
+        debugger
         while (chain.length) {
             promise = promise.then(chain.shift(), chain.shift());
         }
