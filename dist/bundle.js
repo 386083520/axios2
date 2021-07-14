edd82cde24d087f843857e608e5aad232d9f4acf
386083520 <386083520@qq.com>
2021-07-14 22:15:19 +0800

    参数合并

--- dist/bundle.js
+++ dist/bundle.js
@@ -111,11 +111,6 @@ var axios = (function () {
 
     var InterceptorManager_1 = InterceptorManager;
 
-    var mergeConfig = function mergeConfig(config1, config2) {
-        var config = {};
-        return config;
-    };
-
     var toString = Object.prototype.toString;
 
     function extend(a, b, thisArg) {
@@ -139,9 +134,44 @@ var axios = (function () {
         }
     }
 
+    function isUndefined(val) {
+        return typeof val === 'undefined';
+    }
+
     var utils = {
         extend: extend,
-        forEach: forEach
+        forEach: forEach,
+        isUndefined: isUndefined
+    };
+
+    var mergeConfig = function mergeConfig(config1, config2) {
+        var config = {};
+        var valueFromConfig2Keys = ['url', 'method', 'data'];
+        function getMergedValue(target, source) {
+            return source;
+        }
+        function mergeDeepProperties(prop) {
+            if (!utils.isUndefined(config2[prop])) {
+                config[prop] = getMergedValue(config1[prop], config2[prop]);
+            } else if (!utils.isUndefined(config1[prop])) {
+                config[prop] = getMergedValue(undefined, config1[prop]);
+            }
+        }
+        utils.forEach(valueFromConfig2Keys, function valueFromConfig2(prop) {
+            if (!utils.isUndefined(config2[prop])) {
+                config[prop] = getMergedValue(undefined, config2[prop]);
+            }
+        });
+        var axiosKeys = valueFromConfig2Keys;
+        var otherKeys = Object
+            .keys(config1)
+            .concat(Object.keys(config2))
+            .filter(function filterAxiosKeys(key) {
+                return axiosKeys.indexOf(key) === -1;
+            });
+        utils.forEach(otherKeys, mergeDeepProperties);
+        console.log('gsdaaa', config);
+        return config;
     };
 
     function Axios(instanceConfig) {
